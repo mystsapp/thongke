0b27c159886371380dbe0339547f631e7cca4379
ST-PC\ST <mystsapp@gmail.com>
2018-11-21 10:23:40 +0700

    add repository

---
+++ ThongKe/ThongKe.Data/Repositories/accountRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThongKe.Data.Infrastructure;
+using ThongKe.Data.Models;
+
+namespace ThongKe.Data.Repositories
+{
+    public interface IaccountRepository : IRepository<account>
+    {
+
+    }
+    public class accountRepository : RepositoryBase<account>, IaccountRepository
+    {
+        public accountRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
