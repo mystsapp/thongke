c8c0b15987bda084f2a9342beff408d1bcbf781a
ST-PC\ST <mystsapp@gmail.com>
2018-11-23 17:03:31 +0700

    cute

--- ThongKe/ThongKe.Web/App_Start/BundleConfig.cs
+++ ThongKe/ThongKe.Web/App_Start/BundleConfig.cs
@@ -3,47 +3,85 @@ using System.Web.Optimization;
 
 namespace ThongKe.Web
 {
+    //public class BundleConfig
+    //{
+    //    // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+    //    public static void RegisterBundles(BundleCollection bundles)
+    //    {
+    //        bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+    //                    "~/Scripts/jquery-{version}.js"));
+
+    //        bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+    //                    "~/Scripts/jquery.validate*"));
+
+    //        // Use the development version of Modernizr to develop with and learn from. Then, when you're
+    //        // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+    //        bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+    //                    "~/Scripts/modernizr-*"));
+
+    //        bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+    //                  "~/Scripts/bootstrap.js",
+    //                  "~/Scripts/respond.js"));
+
+    //        bundles.Add(new StyleBundle("~/Content/css").Include(
+    //                  //"~/Assets/Admin/vendor/bootstrap/css/bootstrap.min.css",
+    //                  "~/Content/bootstrap.css",
+    //                  "~/Assets/Admin/vendor/metisMenu/metisMenu.min.css",
+    //                  "~/Assets/Admin/js/User/jquery-ui/jquery-ui.css",
+    //                  "~/Assets/Admin/dist/css/sb-admin-2.css",
+    //                  "~/Content/boxover/css/boxover.css",
+    //                  "~/Content/boostrap/vendor/morrisjs/morris.css",
+    //                  "~/Assets/Admin/vendor/fontawesome-free-5.1.0-web/css/fontawesome.css",
+    //                  "~/Assets/Admin/vendor/fontawesome-free-5.1.0-web/css/all.css",
+    //                  "~/Content/Site.css"
+    //                  ));
+
+    //        bundles.Add(new ScriptBundle("~/bundles/vendor").Include(
+    //            "~/Assets/Admin/vendor/jquery/jquery.min.js",
+    //            "~/Assets/Admin/vendor/bootstrap/js/bootstrap.min.js",
+    //                 "~/Assets/Admin/vendor/metisMenu/metisMenu.min.js",
+    //                 "~/Content/boostrap/vendor/raphael/raphael.min.js",
+    //                 "~/Assets/Admin/dist/js/sb-admin-2.js",
+    //                 "~/Content/boxover/js/boxover.js"
+    //                 ));
+    //    }
+    //}
     public class BundleConfig
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                          "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/vendor").Include(
+                     "~/Content/boostrap/vendor/metisMenu/metisMenu.min.js",
+                     "~/Content/boostrap/vendor/raphael/raphael.min.js",
+                     "~/Content/boostrap/dist/js/sb-admin-2.js",
+                     "~/Content/boxover/js/boxover.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Assets/Admin/vendor/bootstrap/css/bootstrap.min.css",
+                      "~/Content/bootstrap.css",
                       "~/Assets/Admin/js/User/jquery-ui/jquery-ui.css",
-                      "~/Assets/Admin/vendor/metisMenu/metisMenu.min.css",
-                      "~/Assets/Admin/dist/css/sb-admin-2.css",
-                      "~/Assets/Admin/vendor/fontawesome-free-5.1.0-web/css/fontawesome.css",
-                      "~/Assets/Admin/vendor/fontawesome-free-5.1.0-web/css/all.css",
-
+                      //"~/Assets/Admin/vendor/fontawesome-free-5.1.0-web/css/all.css",
+                      "~/Content/boostrap/vendor/metisMenu/metisMenu.min.css",
+                      "~/Content/boostrap/dist/css/sb-admin-2.css",
                       "~/Content/boxover/css/boxover.css",
-                      "~/Content/boostrap/vendor/morrisjs/morris.css",
-                      "~/Content/Site.css"
-                      ));
-
-            bundles.Add(new ScriptBundle("~/bundles/vendor").Include(
-                "~/Assets/Admin/vendor/jquery/jquery.min.js",
-                "~/Assets/Admin/vendor/bootstrap/js/bootstrap.min.js",
-                     "~/Assets/Admin/vendor/metisMenu/metisMenu.min.js",
-                     "~/Content/boostrap/vendor/raphael/raphael.min.js",
-                     "~/Assets/Admin/dist/js/sb-admin-2.js",
-                     "~/Content/boxover/js/boxover.js"
-                     ));
+                      "~/Content/boostrap/vendor/morrisjs/morris.css",                      
+                      //"~/Assets/Admin/vendor/fontawesome-free-5.1.0-web/css/all.css",
+                      "~/Content/boostrap/vendor/font-awesome/css/font-awesome.min.css",
+                      "~/Content/Site.css"));
         }
     }
 }
